d75c2ec42a592916a79f4cc0087bb5e90bc92e44
tiff1892 <tiffany.alimusa1892@gmail.com>
2023-08-01 17:30:59 +0800

    added db

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ app.use("/", appRoute);
 
 //to RUN
 const PORT = 3001;
+
+const mysql = require("mysql");
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "tiffburger",
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
+
+  pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+    if (error) throw error;
+    console.log("The solution is: ", results[0].solution);
+  });
 });
